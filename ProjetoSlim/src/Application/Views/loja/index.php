d35017b8b99d438ed521a57cebf2c0d980f7b546
Victor Teixeira <teixeira.ovictor@hotmail.com>
2020-05-23 02:23:13 -0300

    alteração da Index

--- ProjetoSlim/src/Application/Views/loja/index.php
+++ ProjetoSlim/src/Application/Views/loja/index.php
@@ -87,7 +87,7 @@
       </nav>
     </header>
 
-    <div class="site-blocks-cover" style="background-image: url(images/loja_img/pula3.jpg);" data-aos="fade">
+    <div class="site-blocks-cover" style="background-image: url(/images/loja_img/vamosver.jpg);" data-aos="fade">
       <div class="container">
         <div class="row align-items-start align-items-md-center justify-content-end">
           <div class="col-md-5 text-center text-md-left pt-5 pt-md-0">
